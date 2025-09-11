9f64267dde298f104855f69e12ec98d8372881b6
singhpraveen77 <singhpraveen8893@gmail.com>
2025-09-11 21:45:07 +0530

    setup applaoyout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import AppLayout from "./Layout/AppLayout";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import LandingPage from "./Pages/LandingPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { index: true,path:"/", element: <LandingPage /> },     
+      { path: "loginpage", element: <LoginPage /> },  
+      { path: "signupage", element: <SignupPage /> },  
+     
+    ],
+  },  
+]);
 
 function App() {
-  return (
-    <div>
-      Template
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
