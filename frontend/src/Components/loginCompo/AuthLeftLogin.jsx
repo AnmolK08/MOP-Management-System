f7d151808b09741a20f15e3ec9b522ee0e05f43b
singhpraveen77 <singhpraveen8893@gmail.com>
2025-09-17 01:50:45 +0530

    Refactor AuthLeft component: update layout and replace background with image

--- frontend/src/Components/loginCompo/AuthLeftLogin.jsx
+++ frontend/src/Components/loginCompo/AuthLeftLogin.jsx
@@ -1,14 +1,14 @@
+
+
 export default function AuthLeft() {
   return (
-    <div className="hidden lg:flex lg:w-1/2 items-center justify-center relative bg-cover bg-center bg-[url('https://lh3.googleusercontent.com/aida-public/AB6AXuCzCwIDpYlmzjkvWhVskbO4prY_wIHOZF9nd8zgnZ98G36LE0ZZqdMjBd71OVq31HFaF0GdF6LYcZt_azgYXcIGwkwB8MhFoP4yxSTZNgbO0pCRZ1Zk60392cqmSqmbM2mZKpBzDJP9wqJufJhXzpKv2Zo52cTx__EXIN19Y7t2jfpTNRwDKOgMwLJZNgmYeJetajc7RWqBQQp6qIY8nUzf-nH46v-pMCgVz7BODMfWQS3Om2ynZz31oXL_7PR_OrY-WXINnq0_WRM')]">
-      <div className="absolute inset-0 bg-black/40"></div>
-      <div className="relative text-center p-8 text-white">
-        <h1 className="font-serif text-6xl font-bold mb-4">Foodie</h1>
-        <p className="text-xl max-w-md">
-          The heart of your restaurant, from mess to delivery, managed with
-          ease.
-        </p>
-      </div>
+          <div className="flex justify-center items-center">
+      <img
+        src="/Chef.gif"
+        alt="Chef Animation"
+        className="w-90 "
+      />
     </div>
+
   );
 }
