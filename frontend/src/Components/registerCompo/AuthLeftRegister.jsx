b6e3ece52e27033916ee0e18ad64ac891a3e77a8
singhpraveen77 <singhpraveen8893@gmail.com>
2025-09-17 03:02:52 +0530

    Refactor AuthLeft component: simplify layout by removing background and text elements, retaining coffee cup animation

--- frontend/src/Components/registerCompo/AuthLeftRegister.jsx
+++ frontend/src/Components/registerCompo/AuthLeftRegister.jsx
@@ -1,14 +1,9 @@
 export default function AuthLeft() {
   return (
-    <div className="hidden lg:flex lg:w-1/2 items-center justify-center relative bg-cover bg-center bg-[url('https://lh3.googleusercontent.com/aida-public/AB6AXuCzCwIDpYlmzjkvWhVskbO4prY_wIHOZF9nd8zgnZ98G36LE0ZZqdMjBd71OVq31HFaF0GdF6LYcZt_azgYXcIGwkwB8MhFoP4yxSTZNgbO0pCRZ1Zk60392cqmSqmbM2mZKpBzDJP9wqJufJhXzpKv2Zo52cTx__EXIN19Y7t2jfpTNRwDKOgMwLJZNgmYeJetajc7RWqBQQp6qIY8nUzf-nH46v-pMCgVz7BODMfWQS3Om2ynZz31oXL_7PR_OrY-WXINnq0_WRM')]">
-      <div className="absolute inset-0 bg-black/40"></div>
-      <div className="relative text-center p-8 text-white">
-        <h1 className="font-serif text-6xl font-bold mb-4">Foodie</h1>
-        <p className="text-xl max-w-md">
-          The heart of your restaurant, from mess to delivery, managed with
-          ease.
-        </p>
-      </div>
-    </div>
+    <img
+      src="/coffee_cup.gif"
+      alt="Coffee Animation"
+      className="w-100 object-contain"
+    />
   );
 }
