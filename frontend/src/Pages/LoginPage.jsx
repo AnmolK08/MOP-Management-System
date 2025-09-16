2e539d271d0a35f186145dfe364128aba18a7b41
singhpraveen77 <singhpraveen8893@gmail.com>
2025-09-17 01:51:01 +0530

    Refactor LoginPage component: adjust max-width for improved layout

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -4,18 +4,18 @@ import AuthRight from "../Components/loginCompo/AuthRightLogin";
 
 export default function LoginPage() {
   return (
-   <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+  <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
     <div className="relative">
       {/* Background shadow */}
       <div className="absolute inset-0 rounded-2xl bg-black/10 blur-2xl"></div>
 
       {/* Card */}
-      <div className="relative flex w-full max-w-5xl bg-white rounded-2xl shadow-xl overflow-hidden">
+      <div className="relative flex w-full max-w-fit bg-white rounded-2xl shadow-xl overflow-hidden">
         <AuthLeft />
         <AuthRight />
       </div>
     </div>
-</div>
+  </div>
   );
 }
 
