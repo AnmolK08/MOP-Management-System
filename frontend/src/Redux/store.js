b4d46b34103bed3359bbc07aefff052a144a5539
AnmolK08 <mranmolsingh04@gmail.com>
2025-09-17 14:33:12 +0530

    added all initial slices to start

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authSlice from './Slices/authSlice'
+import authSlice from './Slices/authSlice';
+import orderSlice from './Slices/orderSlice';
+import menuSlice from './Slices/menuSlice';
 
 const store = configureStore({
     reducer : {
         authSlice : authSlice,
+        orderSlice : orderSlice,
+        menuSlice : menuSlice,
     }
 })
 
