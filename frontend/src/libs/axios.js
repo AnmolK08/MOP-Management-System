67ee34156220ba829047aec369e681e7b80fed9a
Anmol Kumar <anmol@10.171.19.9>
2025-11-15 23:20:44 +0530

    update axios and socket connection to use a fixed backend URL

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-const backendPort = import.meta.env.PORT || 8080;
-const BACKEND_URL = import.meta.env.BACKEDND_URL || `http://localhost:${backendPort}`;
-console.log("Backend URL:", backendPort);
-
 export const axiosInstance = axios.create({
-    baseURL : BACKEND_URL+"/",
+    baseURL : "https://mop-management-system.onrender.com/",
     withCredentials : true,
 })
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,11 +1,9 @@
 import { io } from "socket.io-client";
 
 let socket;
-const BACKEND_URL = import.meta.env.BACKEND_URL || "http://localhost:8080";
-
 export const initSocket = (user) => {
   
-  socket = io(BACKEND_URL,{
+  socket = io("https://mop-management-system.onrender.com",{
     query:{
       userId:user.userId
     }
